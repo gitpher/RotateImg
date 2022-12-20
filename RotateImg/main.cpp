7e6e434266a8e3572c614d8ba7fcc67a0f142154
gitpher <jun_chois@naver.com>
2022-12-21 00:14:21 +0900

    adding angle variable

--- RotateImg/main.cpp
+++ RotateImg/main.cpp
@@ -3,13 +3,14 @@
 int main(int argc, char* argv[])
 {
     char* imgPath = argv[1];
+    double angle = atof(argv[2]);
 
-    cv::Mat img = cv::imread(imgPath);
-    namedWindow("First OpenCV Application", WINDOW_AUTOSIZE);
-    cv::imshow("First OpenCV Application", img);
-    cv::moveWindow("First OpenCV Application", 0, 45);
-    cv::waitKey(0);
-    cv::destroyAllWindows();
+    Mat img = imread(imgPath);
+    namedWindow(imgPath, WINDOW_AUTOSIZE);
+    imshow(imgPath, img);
+    moveWindow(imgPath, 0, 45);
+    waitKey(0);
+    destroyAllWindows();
 
     return 0;
 }
