93ebbdf9aa295d082fe58a4f7ed154c1355125ad
gitpher <jun_chois@naver.com>
2022-12-20 22:54:28 +0900

    displaying img complete

--- RotateImg/RotateImg.cpp
+++ RotateImg/RotateImg.cpp
@@ -4,9 +4,11 @@
 #include <iostream>
 using namespace cv;
 
-int main()
-{
-    cv::Mat img = cv::imread("C:/a.jpg");
+int main(int argc, char* argv[])
+{   
+    char* imgPath = argv[1];
+
+    cv::Mat img = cv::imread(imgPath);
     namedWindow("First OpenCV Application", WINDOW_AUTOSIZE);
     cv::imshow("First OpenCV Application", img);
     cv::moveWindow("First OpenCV Application", 0, 45);
