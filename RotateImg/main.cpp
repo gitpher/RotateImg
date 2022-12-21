ab86647f0e8a185ca4efb83f36111d09f365ccf8
gitpher <jun_chois@naver.com>
2022-12-21 12:51:14 +0900

    planning

--- RotateImg/main.cpp
+++ RotateImg/main.cpp
@@ -5,17 +5,24 @@ int main(int argc, char* argv[])
     char* imgPath = argv[1];
     double angle = atof(argv[2]);
 
-    Mat img = imread(imgPath);
-    
-    for (int row = 0; row < img.rows; row++)
+    Mat srcImg = imread(imgPath);
+    Mat dstImg = imread(imgPath);
+
+
+    for (int r = 0; r < dstImg.rows; r++)
     {
-        for (int col = 0; col < img.cols; col++)
+        for (int c = 0; c < dstImg.cols; c++)
         {
-            img.at<uint8_t>(row, col);
+            
+            // x = (x)(cos)(seta) - (y)(sin)(seta)
+            // y = (x)(sin)(seta) + (y)(cos)(seta)
+            dstImg.at<uint8_t>(r, c);
         }
     }
     
-    imshow(imgPath, img);
+    // print
+    imshow(imgPath, srcImg);
+    imshow(imgPath, dstImg);
     waitKey(0);
 
     return 0;
