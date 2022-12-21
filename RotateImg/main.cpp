c1f258c6f9dd306dbb1364235971d94a6e2c4efa
gitpher <jun_chois@naver.com>
2022-12-21 19:37:40 +0900

    rotation itself complete

--- RotateImg/main.cpp
+++ RotateImg/main.cpp
@@ -1,4 +1,4 @@
-#include "RotateImg.h"
+﻿#include "RotateImg.h"
 
 int main(int argc, char* argv[])
 {
@@ -7,51 +7,48 @@ int main(int argc, char* argv[])
 
     
     Mat srcImg = imread(imgPath);
+    int hypothenuseOfSrcImg = sqrt(pow(srcImg.rows, 2) + pow(srcImg.cols, 2));
     Mat dstImg(srcImg.rows, srcImg.cols, CV_8UC3, Scalar(0, 0, 0));
 
     double PI = 3.1415926;
     double radian = angle * PI / 180;
 
-    // print original
-    // moving center
-
-
-
-    for (int row = 0; row < dstImg.rows ; row++)
+    for (int row = 0; row < srcImg.rows ; row++)
     {
-        for (int col = 0; col < dstImg.cols; col++)
+        for (int col = 0; col < srcImg.cols; col++)
         {
-            int x = col;
-            int y = row;
+            int x = col - (srcImg.cols / 2);
+            int y = row - (srcImg.rows / 2);
 
-            int xPrime = (x * cos(radian)) + (y * sin(radian));
-            int yPrime = (x * sin(radian)) - (y * cos(radian));
+            int xPrime = (x * cos(radian)) - (y * sin(radian));
+            int yPrime = (x * sin(radian)) + (y * cos(radian));
 
-            int newX = xPrime + (dstImg.cols / 2); // 
-            int newY = yPrime - (dstImg.rows / 2); // 
+            int newRow = yPrime + (srcImg.rows / 2); 
+            int newCol = xPrime + (srcImg.cols / 2);  
 
-            if (newX < 0 || newX >= dstImg.cols)
+            if (newRow < 0 || newRow >= dstImg.rows)
             {
                 continue;
             }
-            if (newY < 0 || newY >= dstImg.rows)
+            if (newCol < 0 || newCol >= dstImg.cols)
             {
                 continue;
             }
 
-            dstImg.at<Vec3b>(newX, newY) = srcImg.at<Vec3b>(x, y);
+            dstImg.at<Vec3b>(newRow, newCol) = srcImg.at<Vec3b>(row, col); 
         }
     }
     
+    // modified의 크기 최적화
+    // tran
 
     namedWindow("ORIGINAL IMAGE", WINDOW_AUTOSIZE);
     imshow("ORIGINAL IMAGE", srcImg);
-    // print modified
     namedWindow("MODIFIED IMAGE", WINDOW_AUTOSIZE);
     imshow("MODIFIED IMAGE", dstImg);
     waitKey(0);
-
     return 0;
 }
 
-// add const
+// const 추가하기 및 최적화
+// 리팩터링
