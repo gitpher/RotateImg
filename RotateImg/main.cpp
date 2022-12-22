8d4c14a6b964629cbfaffd49121ecc0423d87920
gitpher <jun_chois@naver.com>
2022-12-22 09:43:56 +0900

    before interpolation

--- RotateImg/main.cpp
+++ RotateImg/main.cpp
@@ -5,12 +5,17 @@ int main(int argc, char* argv[])
     char* imgPath = argv[1];
     double angle = atof(argv[2]);
 
-    Mat srcImg = imread(imgPath);
-    int hypothenuseOfSrcImg = sqrt(pow(srcImg.rows, 2) + pow(srcImg.cols, 2));
-    Mat dstImg(srcImg.rows, srcImg.cols, CV_8UC3, Scalar(0, 0, 0));
-
     double PI = 3.1415926;
     double radian = angle * PI / 180;
+
+    Mat srcImg = imread(imgPath);
+
+    int newWidth = (srcImg.cols * cos(radian)) + (srcImg.rows * sin(radian));
+    int newHeight = (srcImg.cols * sin(radian)) + (srcImg.rows * cos(radian));
+
+    Mat dstImg(newHeight, newWidth, CV_8UC3, Scalar(0, 0, 0));
+
+    
 
     for (int row = 0; row < srcImg.rows ; row++)
     {
@@ -22,14 +27,14 @@ int main(int argc, char* argv[])
             int xPrime = (x * cos(radian)) - (y * sin(radian));
             int yPrime = (x * sin(radian)) + (y * cos(radian));
 
-            int newRow = yPrime + (srcImg.rows / 2); 
-            int newCol = xPrime + (srcImg.cols / 2);  
+            int newRow = yPrime + (newHeight / 2); 
+            int newCol = xPrime + (newWidth / 2);  
 
-            if (newRow < 0 || newRow >= dstImg.rows)
+            if (newRow < 0 || newRow >= newHeight)
             {
                 continue;
             }
-            if (newCol < 0 || newCol >= dstImg.cols)
+            if (newCol < 0 || newCol >= newWidth)
             {
                 continue;
             }
