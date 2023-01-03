d3d66912fa473d2547695d89f93725a7bd8f324c
gitpher <jun_chois@naver.com>
2023-01-03 09:40:20 +0900

    adding extern "C" __declspect in order to export as dll

--- RotateImg/RotateImg.h
+++ RotateImg/RotateImg.h
@@ -13,11 +13,11 @@ using namespace cv;
 
 const double PI = 3.14159265358979323846;
 
-double getRadian(double angle);
-Mat createDstImg(Mat& srcImg, double angle);
-double findMax(double arr[], int cnt);
-double findMin(double arr[], int cnt);
-void fillDstImg(Mat& dstImg, Mat& srcImg, double radian);
-bool isOutOfBounds(Mat& srcImg, double srcRow, double srcCol);
-void interpolateDstImg(Mat& dstImg, Mat& srcImg, int dstRow, int dstCol, double srcRow, double srcCol);
-void displayImage(Mat& image, const string displayName);
+extern "C" __declspec(dllexport) double getRadian(double angle);
+extern "C" __declspec(dllexport) Mat createDstImg(Mat& srcImg, double angle);
+extern "C" __declspec(dllexport) double findMax(double arr[], int cnt);
+extern "C" __declspec(dllexport) double findMin(double arr[], int cnt);
+extern "C" __declspec(dllexport) void fillDstImg(Mat& dstImg, Mat& srcImg, double radian);
+extern "C" __declspec(dllexport) bool isOutOfBounds(Mat& srcImg, double srcRow, double srcCol);
+extern "C" __declspec(dllexport) void interpolateDstImg(Mat& dstImg, Mat& srcImg, int dstRow, int dstCol, double srcRow, double srcCol);
+extern "C" __declspec(dllexport) void displayImage(Mat& image, const string displayName);
